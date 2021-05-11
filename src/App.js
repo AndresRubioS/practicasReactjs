1176612127108a8c69faa8550e219262f3f76ec4
AndresRubioS <andres-rubio-salas@hotmail.com>
2021-05-11 14:04:36 +0200

    prueba

--- src/App.js
+++ src/App.js
@@ -6,9 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <nav class="anchototal">
+    <a href="#">Portada</a>
+    <a href="#punk-rock">Punk-rock</a>
+    <a href="#fundadores">Fundadores</a>
+    <a href="#baterias">Baterias</a>
+    <a href="#discografia">Discografía</a>
+  </nav>
         <p>
-          hola fljruiñogzriyñirygñrghuñrgiugñrgigyurfgyi
+          hola fljruiñogzriyñirygñrghuñrgiugñrgigyurfgyis
         </p>
+        <p>sws</p>
         <a
           className="App-link"
           href="https://reactjs.org"
